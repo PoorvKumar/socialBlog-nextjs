26bfc1fea62b764d79c44ec5a817b9c98f4f682f
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-19 11:32:54 +0530

    links in navbar && footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,32 +14,32 @@ const Footer = () => {
         <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quae odit atque reprehenderit harum vitae sit accusantium expedita veritatis quisquam!</p>
         <div className={styles.icons}>
         <Link href="/"><Image src="/youtube.png" alt='youtube' width={18} height={18}/></Link>
-        <Link href="/"><Image src="/twitter.png" alt='twitter' width={18} height={18}/></Link>
-        <Link href="/"><Image src="/instagram.png" alt='instagram' width={18} height={18}/></Link>
+        <a href="https://twitter.com/PoorvKumar1" target='_blank'><Image src="/twitter.png" alt='twitter' width={18} height={18}/></a>
+        <a href="https://www.instagram.com/poorv1/" target='_blank'><Image src="/instagram.png" alt='instagram' width={18} height={18}/></a>
         <Link href="/"><Image src="/facebook.png" alt='facebook' width={18} height={18}/></Link>
         </div>
       </div>
       <div className={styles.links}>
         <div className={styles.list}>
           <span className={styles.listTitle}>Social</span>
-          <Link href="/">Github</Link>
-          <Link href="/">Linkedin</Link>
-          <Link href="/">Twitter</Link>
+          <a href="https://github.com/PoorvKumar/socialBlog-nextjs" target='_blank'>Github</a>
+          <a href="https://www.linkedin.com/in/PoorvKumar" target='_blank'>Linkedin</a>
+          <a href="https://twitter.com/PoorvKumar1" target='_blank'>Twitter</a>
           <Link href="/">socialBlog</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Links</span>
           <Link href="/">Home</Link>
-          <Link href="/">Blog</Link>
+          <Link href="/blog?cat=travel">Blog</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
         </div>
         <div className={styles.list}>
           <span className={styles.listTitle}>Tags</span>
-          <Link href="/">Style</Link>
-          <Link href="/">Fashion</Link>
-          <Link href="/">Coding</Link>
-          <Link href="/">Travel</Link>
+          <Link href="/blog?cat=style">Style</Link>
+          <Link href="/blog?cat=travel">Travel</Link>
+          <Link href="/blog?cat=coding">Coding</Link>
+          <Link href="/blog?cat=culture">Culture</Link>
         </div>
       </div>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.social}>
         <Link href="/"><Image src="/youtube.png" alt='youtube' width={24} height={24}/></Link>
-        <Link href="/"><Image src="/twitter.png" alt='twitter' width={24} height={24}/></Link>
-        <Link href="/"><Image src="/instagram.png" alt='instagram' width={24} height={24}/></Link>
+        <a href="https://twitter.com/PoorvKumar1" target='_blank'><Image src="/twitter.png" alt='twitter' width={24} height={24}/></a>
+        <a href="https://www.instagram.com/poorv1/" target='_blank'><Image src="/instagram.png" alt='instagram' width={24} height={24}/></a>
         <Link href="/"><Image src="/facebook.png" alt='facebook' width={24} height={24}/></Link>
       </div>
       <div className={styles.logo}><Link href={"/"}>socialBlog</Link></div>
