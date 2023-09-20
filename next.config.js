1ae85e52e94ba5e664ecb9a59245fdb81eff6443
Poorv Kumar <55318092+PoorvKumar@users.noreply.github.com>
2023-09-20 13:58:05 +0530

    discord image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       "lh3.googleusercontent.com",
       "firebasestorage.googleapis.com",
       "avatars.githubusercontent.com",
+      "cdn.discordapp.com",
     ],
   }
 };
